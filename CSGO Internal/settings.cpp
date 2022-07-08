052de3025357dab6755eb5357f35f00640552471
RegiSimkus <regi.simkus2@gmail.com>
2022-07-08 13:58:59 +0100

    added settings

---
+++ CSGO Internal/settings.cpp
@@ -0,0 +1,11 @@
+#include "settings.h"
+
+namespace Settings
+{
+	namespace Aim
+	{
+		bool Enabled = false;
+		float FOV = 0.f;
+		float FOVLength = 0.f; // The length of the hypotenuse/size of radius
+	}
+}
---
+++ CSGO Internal/settings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Settings
+{
+	namespace Aim
+	{
+		extern bool Enabled;
+		extern float FOV;
+		extern float FOVLength; // The length of the hypotenuse/size of radius
+	}
+}
