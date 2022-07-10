140ba3bcd5c1b6695de82b17d43bd4875f185979
RegiSimkus <regi.simkus2@gmail.com>
2022-07-10 13:31:17 +0100

    prettier ESP using ImGui

--- CSGO Internal/playeresp.cpp
+++ CSGO Internal/playeresp.cpp
@@ -1,51 +1,129 @@
 #include "esp.h"
+#include <math.h>
+
+// To rotate around a constant axis
+// the originalAngle will always be the same here
+void Rotate(Vector& pivot, Vector& out, float angle)
+{
+
+	float distance = pivot.Distance2D(out);
+
+	float rad = RAD(angle);
+
+	float c = cos(rad);
+	float s = sin(rad);
+
+	out.x = pivot.x + (c * distance);
+	out.y = pivot.y + (s * distance);
+}
 
 void ESP::DrawPlayer(C_BasePlayer* player, C_BasePlayer* localplayer, player_info_t playerinfo)
 {
-	bool bSameTeam = player->GetTeam() == localplayer->GetTeam();
+	if (player == localplayer)
+		return;
+
+	if (player->GetHealth() <= 0)
+		return;
+
+	if (player->IsDormant())
+		return;
 
 	matrix3x4_t bones[128];
 	if (!player->SetupBones(bones, 128, 0x7FF00, g_Globals->curtime))
 		return;
 
-	static Vector VECTOR_HEADOFFSET = Vector(0, 0, 14);
-	static Vector VECTOR_FEETOFFSET = Vector(0, 0, -8);
+	bool bSameTeam = player->GetTeam() == localplayer->GetTeam();
+	ImColor col = bSameTeam ? ImColor_Green : ImColor_Red;
 
-	Vector feetScreenPos;
-	Vector feetPos = player->GetABSOrigin();
-	feetPos = feetPos + VECTOR_FEETOFFSET;
-	if (I::IDebugOverlay->ScreenPosition(feetPos, feetScreenPos))
-		return;
+	Vector pos = player->GetABSOrigin();
 
-	Color col = bSameTeam ? COLOR_GREEN : COLOR_RED;
+	Vector center = pos + player->GetCollisionProperties()->Center();
+	Vector mins = pos + player->GetCollisionProperties()->mins;
+	Vector maxs = pos + player->GetCollisionProperties()->maxs;
 
-	Vector headScreenPos;
-	Vector headPos = bones[8].GetOrigin();
-	headPos = headPos + VECTOR_HEADOFFSET;
+	Vector tl = Vector(center.x, mins.y, maxs.z);
+	Vector tr = Vector(center.x, maxs.y, maxs.z);
 
-	I::IDebugOverlay->ScreenPosition(headPos, headScreenPos);
+	Vector br = Vector(center.x, maxs.y, mins.z);
+	Vector bl = Vector(center.x, mins.y, mins.z);
 
-
-	float height = feetScreenPos.y - headScreenPos.y;
-	float width = height * 0.44;
-
-	float offset = (width / 2);
-
-	float left = headScreenPos.x - offset;
-	float top = headScreenPos.y;
-	float right = headScreenPos.x + offset;
-	float bottom = headScreenPos.y + height;
-
-	I::ISurface->SetDrawColor(col.r, col.g, col.b, 255);
-
-	I::ISurface->DrawLine(left, top, right, top);
-	I::ISurface->DrawLine(right, top, right, bottom);
-
-	I::ISurface->DrawLine(right, bottom, left, bottom);
-	I::ISurface->DrawLine(left, bottom, left, top);
+	// screen positions
+	Vector topRight, topLeft, bottomLeft, bottomRight;
 
 
-	int textWidth, textHeight;
-	I::ISurface->GetTextSize(293, playerinfo.name, textWidth, textHeight);
-	I::ISurface->DrawPrintText(playerinfo.name, left + (((right - left)/2) - textWidth/2), feetScreenPos.y, 293, col);
+	Vector localPos = localplayer->GetOrigin() + localplayer->GetViewOffset();
+	Vector angles = (center - localPos).Angle();
+
+	// +/- 90 because the corners are relative
+	Rotate(pos, tr, angles.y - 90.f);
+	Rotate(pos, br, angles.y - 90.f);
+	Rotate(pos, tl, angles.y + 90.f);
+	Rotate(pos, bl, angles.y + 90.f);
+
+	I::IDebugOverlay->ScreenPosition(tr, topRight);
+	I::IDebugOverlay->ScreenPosition(tl, topLeft);
+	I::IDebugOverlay->ScreenPosition(br, bottomRight);
+	I::IDebugOverlay->ScreenPosition(bl, bottomLeft);
+
+	// top
+	DrawLine(topLeft.x, topLeft.y, topRight.x, topRight.y, 1, col);
+
+	// left
+	DrawLine(topLeft.x, topLeft.y, bottomLeft.x, bottomLeft.y, 1, col);
+
+	// bottom
+	DrawLine(bottomLeft.x, bottomLeft.y, bottomRight.x, bottomRight.y, 1, col);
+
+	// right
+	DrawLine(bottomRight.x, bottomRight.y, topRight.x, topRight.y, 1, col);
+
+	float width = bottomRight.x - bottomLeft.x;
+	float height = bottomLeft.y - topLeft.y;
+
+	float& x = bottomLeft.x;
+
+	float& top = topRight.y;
+
+	float& bottom = bottomLeft.y;
+	bottom += 1.f; // margin to separate lines & text
+
+	float& right = bottomRight.x;
+	right += 1.f;
+
+	float cx = bottomLeft.x + (bottomRight.x - bottomLeft.x) / 2.f;
+	float cy = bottomLeft.y + (bottomRight.y - bottomLeft.y) / 2.f;
+
+	DrawOutlinedText(playerinfo.name, cx, cy, 1, ImColor_White, true, false);
+
+	{
+		float bx = bottomRight.x + 5.f;
+		float by = bottomRight.y;
+		
+		float tx = topRight.x + 5.f;
+
+		float healthRatio = player->GetHealth() / 100.f;
+
+		// the ratio used to determine the end of the bar
+		float ratio = 1.f - healthRatio;
+		
+		float barX = topRight.x + 5.f + (ratio * (bx - (topRight.x + 5.f)));
+		float barY = topRight.y +		(ratio * (by - topRight.y));
+
+		DrawLine(tx, topRight.y, bx, by, 4.f, ImColor_Black);
+
+		ImColor healthCol = ImColor(
+			(UINT)((ratio) * 255.f),
+			(UINT)(healthRatio * 255.f),
+			0
+		);
+
+		DrawLine(
+			barX,
+			barY,
+			bx,
+			by,
+			2.f,
+			healthCol
+		);
+	}
 }
