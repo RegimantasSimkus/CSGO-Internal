d0502db42346724ad3e473ce59edae93c0abd2b0
RegiSimkus <regi.simkus2@gmail.com>
2022-07-10 13:36:36 +0100

    prevented fovcircle from rendering when it doesn't need to

--- CSGO Internal/fovcircle.cpp
+++ CSGO Internal/fovcircle.cpp
@@ -3,12 +3,14 @@
 
 void ESP::DrawFOVCircle(C_BasePlayer* localplayer)
 {
-	if (!localplayer) return;
+	if (Settings::Aim::FOV == 0.f || !Settings::Aim::Enabled)
+		return;
 
+	if (!localplayer) return;
 
 	float radius = GetFOVRadius(Settings::Aim::FOV, localplayer);
 
-	// Validating FOVLength in case the FOV was set while the localplayer was null
+	// Validating FOVLength in case the FOV was set while the localplayer was null, such as in the main menu
 	if (Settings::Aim::FOVLength == 0.f && Settings::Aim::FOV != 0.f)
 		Settings::Aim::FOVLength = radius * radius;
 
